702544aeb7f08d84102d8e9e01e376ab0e5f1598
Suraj Singh <singh_suraj@controlprint.com>
2026-07-15 17:52:35 +0530

    vector testing

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-from pathlib import Path
+import math
 
-document = Path("data/company").read_text(encoding="utf-8")
+A = [1, 2]
+B = [2, -1]
 
-print(document)
+
+dot = sum(a * b for a, b in zip(A, B))
+
+mag_a = math.sqrt(sum(a * a for a in A))
+mag_b = math.sqrt(sum(b * b for b in B))
+
+cosine = dot / (mag_a * mag_b)
+
+print(cosine)
