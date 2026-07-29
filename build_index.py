18146089ade3e36843e3274442d8e35d4ad7bb46
skcse47 <skcse47@gmail.com>
2026-07-29 14:38:03 +0530

    build change

---
+++ build_index.py
@@ -0,0 +1,31 @@
+from rag.chunker import chunk_text
+from rag.embedder import create_embeddings
+from rag.vector_store import save_vector_store
+
+
+with open("data/company", "r", encoding="utf-8") as file:
+    text = file.read()
+
+chunks = chunk_text(text)
+
+embeddings = create_embeddings(chunks)
+
+vector_store = []
+
+for idx, (chunk, embedding) in enumerate(zip(chunks, embeddings)):
+
+    vector_store.append({
+        "id": idx,
+        "text": chunk,
+        "embedding": embedding,
+        "metadata": {
+            "source": "company.txt"
+        }
+    })
+
+save_vector_store(
+    vector_store,
+    "storage/vector_store.json"
+)
+
+print("Index created successfully.")
