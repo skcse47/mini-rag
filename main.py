b4c95dcdbce13ba09f45dce3c4c16a32196c633e
skcse47 <skcse47@gmail.com>
2026-08-07 22:35:24 +0530

    faiss implemented

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from rag.embedder import create_embeddings
-from rag.vector_store import load_vector_store
-from rag.retriever import retrieve
+from rag.retriever import retrieve, retrieve_with_faiss
 from rag.llm import generate_answer
 from rag.config import TOP_K
 from rag.prompt import build_rag_prompt
 
 
-
-vector_store = load_vector_store(
-    "storage/vector_store.json"
-)
-
 query = input("Ask a question: ")
 
 query_embedding = create_embeddings(query)[0]
 
-results = retrieve(
+# results = retrieve(
+#     query_embedding,
+#     vector_store,
+#     top_k=TOP_K
+# )
+
+results = retrieve_with_faiss(
     query_embedding,
-    vector_store,
     top_k=TOP_K
 )
 
@@ -25,9 +24,6 @@ context = "\n".join(
         result["record"]["text"]
         for result in results
 )
-print("\n===== Retrieved Context =====")
-print(context)
-print("=============================\n")
 
 prompt = build_rag_prompt(context, query)
 answer = generate_answer(prompt)
